9a48b24fb577f395bfacd947c783febe84bf5dce
Joelson J Santos <joelsonti@outlook.com>
2026-05-05 22:46:27 -0300

    Exclusão main

--- js/script.js
+++
@@ -1,6 +0,0 @@
-const menuBtn = document.getElementById("menuBtn");
-const menu = document.getElementById("menu");
-
-menuBtn.addEventListener("click", () => {
-  menu.classList.toggle("active");
-});
